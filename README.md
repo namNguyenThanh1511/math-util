16b842e268f0ca423d206345f62e32a47cdf82a2
namNguyenThanh1511 <nam151103@gmail.com>
2024-09-08 20:29:14 +0700

    Refactor getFactorial() with recursion

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 ### You can find in this repo these following stuff : 
 * The Math-Util library that offers multiple math functions ( a Java-based source code )
 * Unit Testing source code with JUnit framework 
+* Github Actions to demonstrate the Continuous Integration ( CI ) technique
+* Regression Test
 * ...
 ### Screenshots:
 ![DDT & TDD with JUnit](https://github.com/namNguyenThanh1511/math-util/blob/main/images/DDT%20with%20JUnit.png) 
--- src/com/makisekurisu/mathutil/core/MathUtil.java
+++ src/com/makisekurisu/mathutil/core/MathUtil.java
@@ -23,6 +23,19 @@ public class MathUtil {
     // 20! : 18 số 0 , vừa kịp đủ cho kiểu long của Java
     //21! -> tràn kiểu long 
     //bài này quy ước tính n! trong khoảng từ 0..20
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20) {
+//            throw new IllegalArgumentException(n < 0 ? "Cannot calculate , number must > 0 " : "Cannot calculate  21! or above ");
+//        }
+//        if (n == 0 || n == 1) {
+//            return 1;
+//        }
+//        long product = 1;
+//        for (int i = 2; i <= n; i++) {
+//            product *= i;
+//        }
+//        return product;
+//    }
     public static long getFactorial(int n) {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException(n < 0 ? "Cannot calculate , number must > 0 " : "Cannot calculate  21! or above ");
@@ -30,10 +43,13 @@ public class MathUtil {
         if (n == 0 || n == 1) {
             return 1;
         }
-        long product = 1;
-        for (int i = 2; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        return n * getFactorial(n - 1);
     }
+    // tính dùm tui 6! :
+    //n! = 1.2.3.4.....n
+    //6!=  6*5!
+    //5! = 5* 4!
+    //....
+    //n! = n*(n-1)!
+
 }
